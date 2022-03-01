cf55582e6877fb60c0cc6c2c7270b42a0694bb97
Aaliya Nafis <aliyajavascript@gmail.com>
2022-03-01 18:48:09 +0530

    completed assignment of values and variables

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
 console.log("alia")
 
-let firstName = "alia"
+let firstName = "aaliya"
 console.log (firstName)
+
+let myFirstJob = "Programmer"
+let myCurrentJob = "Teacher"
+
+console.log (myFirstJob)
+
+//completed the assignment of values and variables
+
+let contry = "india";
+let continent = "asia";
+let population = 1300000000;
+
+console.log (contry);
+console.log (continent);
+console.log (population);
